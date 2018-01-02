7ccee31d15a0170b782fbc5410f52d730c72ce93
franciscoquijada <francisco_quijada@outlook.com>
2018-01-02 15:33:37 -0300

    Creando migracion de posts

---
+++ database/migrations/2018_01_02_181016_posts.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id')->unsigned();
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('except');
+            $table->string('body');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
